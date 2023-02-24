53dca478880b0f4b4eb3374ab3400e8eeee89c79
Cameron <cameron.morgan@skiff.com>
2023-02-24 16:54:56 -0600

    Completed the 'Posts' component.

--- src/components/Posts/PostHeader.js
+++ src/components/Posts/PostHeader.js
@@ -14,7 +14,7 @@ const PostHeader = props => {
           src={thumbnailUrl}
         />
       </div>
-      <h2>{username}</h2>
+      <h2>@{username}</h2>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,12 +5,17 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  
   return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
+    posts.map(post => {
+      return (
+        <div className='posts-container-wrapper'>
+          {/* Map through the posts array returning a Post component at each iteration */}
+          {/* Check the implementation of Post to see what props it requires! */}
+          <Post likePost={likePost} post={post} key={post.id} />
+        </div>
+      )
+    })
   );
 };
 
